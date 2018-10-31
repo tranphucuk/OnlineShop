814e874a30b03d4281275dd4d7e4af4c1bec1d50
TRAN <p.n.tran999@gmail.com>
2018-10-31 14:33:53 +0000

    Write method (to get entity) and dispose method

---
+++ OnlineShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private OnlineShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public OnlineShopDbContext DbContext
+        {
+            get
+            {
+                return dbContext ?? (dbContext = dbFactory.Init());
+            }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
