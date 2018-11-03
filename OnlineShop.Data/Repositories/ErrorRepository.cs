b02e906557f243a1a99fccd2491ceb1c8a37c809
TRAN <p.n.tran999@gmail.com>
2018-11-03 18:04:43 +0000

    Fix Error(2)

--- OnlineShop.Data/Repositories/ErrorRepository.cs
+++ OnlineShop.Data/Repositories/ErrorRepository.cs
@@ -1,16 +1,23 @@
 ﻿using OnlineShop.Data.Infrastructure;
 using OnlineShop.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace OnlineShop.Data.Repositories
 {
     public interface IErrorRepository : IRepository<Error>
     {
+
     }
 
-    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    public class ErrorRepository : RepositoryBase<Error>
     {
         public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
         {
+
         }
     }
 }
