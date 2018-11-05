1a48b4548af50c2edde670f65bde1ddbb044d696
TRAN <p.n.tran999@gmail.com>
2018-11-05 16:52:01 +0000

    *** Update : ASP>NET Identity

---
+++ OnlineShop.Model/Model/ApplicationUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Model.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(256)]
+        public string Fullname { get; set; }
+
+        [MaxLength(256)]
+        public string Address { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
---
+++ OnlineShop.Web/API/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using OnlineShop.Service;
+using OnlineShop.Web.App_Start;
+using OnlineShop.Web.Infrastructure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace OnlineShop.Web.API
+{
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiControllerBase
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AccountController(IErrorService errorService, ApplicationUserManager userManager, ApplicationSignInManager signInManager) : base(errorService)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.Current.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("login")]
+        public async Task<HttpResponseMessage> Login(HttpRequestMessage request, string username, string password, bool rememberMe)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await SignInManager.PasswordSignInAsync(username, password, rememberMe, shouldLockout: false);
+                return request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            return request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+    }
+}
