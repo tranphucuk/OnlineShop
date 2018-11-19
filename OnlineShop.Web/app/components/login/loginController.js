0a5627cce482c56e4c9c6fa2ba2c24700cb8dfd3
TRAN <p.n.tran999@gmail.com>
2018-11-19 20:21:08 +0000

    ***Update

---
+++ OnlineShop.Web/app/components/login/loginController.js
@@ -0,0 +1,12 @@
+﻿(function (app) {
+    app.controller('loginController', loginController);
+
+    loginController.$inject = ['$scope', '$state']
+
+    function loginController($scope, $state) {
+        $scope.loginSubmit = function () {
+            $state.go('home')
+
+        };
+    };
+})(angular.module('onlineShop'));
