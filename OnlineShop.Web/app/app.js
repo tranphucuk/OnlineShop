88f908c00203133ce7e32d49cbcc4b272956785c
TRAN <p.n.tran999@gmail.com>
2018-11-19 20:19:03 +0000

    Build 'login page' and restructure admin site, app.js, and using 'nested' view of ui-router to separate home and login page

--- OnlineShop.Web/app/app.js
+++ OnlineShop.Web/app/app.js
@@ -2,18 +2,33 @@
 (function () {
     angular.module("onlineShop",
         ['onlineShop.products',
-        'onlineShop.productCategory',
-        'onlineShop.common'])
+            'onlineShop.productCategory',
+            'onlineShop.common'])
         .config(config);
 
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('home', {
-            url: '/admin',
-            templateUrl: '/app/components/home/homeView.html',
-            controller: 'homeController',
-        })
+        $stateProvider
+
+            .state('base', {
+                url: '',
+                templateUrl: '/app/shared/baseView/baseView.html',
+                abstract: true
+            })
+
+            .state('login', {
+                url: '/login',
+                templateUrl: '/app/components/login/loginView.html',
+                controller: 'loginController',
+            })
+
+            .state('home', {
+                url: '/admin',
+                parent: 'base',
+                templateUrl: '/app/components/home/homeView.html',
+                controller: 'homeController',
+            })
         $urlRouterProvider.otherwise('/admin');
     }
 })();
--- OnlineShop.Web/app/components/products/products.module.js
+++ OnlineShop.Web/app/components/products/products.module.js
@@ -8,16 +8,19 @@
 
         $stateProvider.state('productList', {
             url: '/product_list',
+            parent: 'base',
             templateUrl: '/app/components/products/productListView.html',
             controller: 'productListController',
 
         }).state('productAdd', {
             url: '/product_add',
+            parent: 'base',
             templateUrl: '/app/components/products/productAddView.html',
             controller: 'productAddController',
 
         }).state('productEdit', {
             url: '/product_edit/:id',
+            parent: 'base',
             templateUrl: '/app/components/products/productEditView.html',
             controller: 'productEditController',
         });
