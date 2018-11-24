b8e8550228088f99ed901c1ee8b259f2944100f4
TRAN <p.n.tran999@gmail.com>
2018-11-24 00:03:41 +0000

    Setup asp.net identity

---
+++ OnlineShop.Web/API/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using OnlineShop.Service;
+using OnlineShop.Web.Infrastructure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineShop.Web.API
+{
+    [RoutePrefix("api/home")]
+    [Authorize]
+    public class HomeController : ApiControllerBase
+    {
+        IErrorService _errorService;
+        public HomeController(IErrorService errorService) : base(errorService)
+        {
+            this._errorService = errorService;
+        }
+
+        [HttpGet]
+        [Route("TestMethod")]
+        public string TestMethod()
+        {
+            return "Hello";
+        }
+    }
+}
